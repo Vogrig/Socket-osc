72c4f91b81cc054f59920f7f788fa54af6d9a68c
Vogrig <vogrig.andrea@gmail.com>
2015-02-10 23:35:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,32 +3,34 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var PORT = process.env.PORT || 8080;
+var checked = [16];
 
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/osc.html');
 });
 
 io.on('connection',function(socket){
-	console.log("user id : %s",socket.id);
-	socket.on("config", function (obj) {
-    oscServer = new osc.Server(obj.server.port, obj.server.host);
-    oscClient = new osc.Client(obj.client.host, obj.client.port);
-
-    oscClient.send('/status', socket.sessionId + ' connected');
-
-    oscServer.on('message', function(msg, rinfo) {
-      console.log(msg, rinfo);
-      socket.emit("message", msg);
-    });
+    console.log("user id : %s",socket.id);
+    socket.on("config", function (obj) {
+    	io.sockets.emit("update", checked);
+    	oscServer = new osc.Server(obj.server.port, obj.server.host);
+    	oscClient = new osc.Client(obj.client.host, obj.client.port);
+    	oscClient.send('/status', socket.sessionId + ' connected');
+    	oscServer.on('message', function(msg, rinfo) {
+      		console.log(msg, rinfo);
+      		socket.emit("message", msg);
+    	});
   });
-  socket.on("message", function (address,arg1,arg2,arg3) {
-    oscClient.send(address,arg1,arg2,arg3);
+  
+  socket.on("message", function (address,arg1,arg2) {
+    	checked[arg1]=arg2;
+    	io.sockets.emit("update", checked);
+    	oscClient.send(address,arg1,arg2);
   });
 
   socket.on('disconnect',function(){
 		console.log("Disconnect : %s",socket.id);
 	});
-
 
 });
 
