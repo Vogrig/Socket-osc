3a958201c02dddfcb2a21cc713f25d1d4a7672b9
Andrea Vogrig <vogrig.andrea@gmail.com>
2015-02-11 01:10:27 +0100

    Update

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var PORT = process.env.PORT || 8080;
+<<<<<<< Updated upstream
 var oscServer,oscClient;
 var checked = [16];
+=======
+var checked = [16];
+var oscServer,oscClient;
+>>>>>>> Stashed changes
 
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/osc.html');
 });
 
 io.on('connection',function(socket){
+<<<<<<< Updated upstream
     console.log("user id : %s",socket.id);
     socket.on("config", function (obj) {
     	io.sockets.emit("update", checked);
@@ -27,6 +33,29 @@ io.on('connection',function(socket){
     	checked[arg1]=arg2;
     	io.sockets.emit("update", checked);
     	oscClient.send(address,arg1,arg2);
+=======
+	console.log("user id : %s",socket.id);
+	socket.on("config", function (obj) {
+    io.sockets.emit("update", checked);
+    oscServer = new osc.Server(obj.server.port, obj.server.host);
+    oscClient = new osc.Client(obj.client.host, obj.client.port);
+
+    oscClient.send('/status', socket.sessionId + ' connected');
+    
+    oscServer.on('message', function(msg, rinfo) {
+      console.log(msg, rinfo);
+      socket.emit("message", msg);
+    });
+  
+  });
+
+ 
+
+  socket.on("message", function (address,arg1,arg2) {
+    checked[arg1]=arg2;
+    io.sockets.emit("update", checked);
+    oscClient.send(address,arg1,arg2);
+>>>>>>> Stashed changes
   });
 
   socket.on('disconnect',function(){
