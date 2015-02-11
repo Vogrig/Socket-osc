ed98e113648ab58bdee648b5e610e8aab17ee190
Vogrig <vogrig.andrea@gmail.com>
2015-02-11 10:53:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var PORT = process.env.PORT || 8080;
-
 var oscServer,oscClient;
 var checked = [16];
-
-var checked = [16];
-var oscServer,oscClient;
 
 
 app.get('/',function(req,res){
@@ -29,28 +25,6 @@ io.on('connection',function(socket){
     	});
   });
   
-  socket.on("message", function (address,arg1,arg2) {
-    	checked[arg1]=arg2;
-    	io.sockets.emit("update", checked);
-    	oscClient.send(address,arg1,arg2);
-
-	console.log("user id : %s",socket.id);
-	socket.on("config", function (obj) {
-    io.sockets.emit("update", checked);
-    oscServer = new osc.Server(obj.server.port, obj.server.host);
-    oscClient = new osc.Client(obj.client.host, obj.client.port);
-
-    oscClient.send('/status', socket.sessionId + ' connected');
-    
-    oscServer.on('message', function(msg, rinfo) {
-      console.log(msg, rinfo);
-      socket.emit("message", msg);
-    });
-  
-  });
-
- 
-
   socket.on("message", function (address,arg1,arg2) {
     checked[arg1]=arg2;
     io.sockets.emit("update", checked);
